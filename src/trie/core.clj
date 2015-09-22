494e57467d8a394fee4caf3eb8b4664cf2f5c0c0
worace <horace.d.williams@gmail.com>
2015-09-21 19:01:20 -0600

    add insertion at words retrieval

--- src/trie/core.clj
+++ src/trie/core.clj
@@ -1,13 +1,24 @@
 (ns trie.core)
 
-(defn alphabet []
-  (map char (range 97 123)))
-
 (defn trie
   ([] (trie false))
   ([word?] {:children {} :word? word?}))
 
 (defn insert [t [char & rest]]
-  (if (empty? rest)
-    (assoc-in t [:children char] (assoc (trie) :word? true))
-    (assoc-in t [:children char] (insert (trie) rest))))
+  (let [node (or (get-in t [:children char]) (trie))]
+    (if (empty? rest)
+      (assoc-in t [:children char] (assoc node :word? true))
+      (assoc-in t [:children char] (insert node rest)))))
+
+(defn words
+  ([t] (map #(apply str %) (words t [])))
+  ([t path]
+  (concat
+   (if (:word? t) [path] [])
+   (mapcat (fn [[char child]]
+             (words child (conj path char)))
+           (:children t)))))
+
+(defn insert-words
+  ([words] (insert-words (trie) words))
+  ([t words] (reduce insert t words)))
--- test/trie/core_test.clj
+++ test/trie/core_test.clj
@@ -2,11 +2,6 @@
   (:require [clojure.test :refer :all]
             [trie.core :refer :all]))
 
-(deftest test-alphabet
-  (is (= 26 (count (alphabet))))
-  (is (= \a (first (alphabet))))
-  (is (= \z (last (alphabet)))))
-
 (deftest test-trie
   (testing "generates new map"
     (is (= #{:children :word?} (into #{} (keys (trie)))))))
@@ -27,3 +22,47 @@
       (is (get-in t [:children \a :children \s]))
       (is (not (get-in t [:children \a :word?])))
       (is (get-in t [:children \a :children \s :word?])))))
+
+(deftest test-inserting-additional-words
+  (testing "leaves existing words intact"
+    (let [t (insert (insert (trie) "a") "as")
+          t2 (insert t "ask")]
+      (is (get-in t [:children \a :children \s]))
+      (is (get-in t [:children \a :word?]))
+      (is (get-in t [:children \a :children \s :word?]))
+      (is (get-in t2 [:children \a :word?]))
+      (is (get-in t2 [:children \a :children \s :word?]))
+      (is (get-in t2 [:children \a :children \s :children \k :word?])))))
+
+(deftest test-inserting-shorter-second-word
+  (testing "marks intermediate node as word?"
+    (let [t (insert (insert (trie) "ask") "as")
+          t2 (insert t "a")]
+      (is (get-in t [:children \a :children \s :children \k :word?]))
+      (is (get-in t [:children \a :children \s :word?]))
+      (is (get-in t2 [:children \a :word?])))))
+
+(deftest test-words
+  (testing "retrieves all words out of the tree"
+    (let [t (insert (trie) "a")]
+      (is (= ["a"] (words t)))
+      (is (= ["a" "as"] (words (insert t "as")))))))
+
+(deftest test-insert-words
+  (testing "takes trie and coll of words and inserts all, returning the trie"
+    (let [t (insert-words (trie) ["a" "as" "ask"])]
+      (is (:word? (get-in t [:children \a :children \s :children \k]))))))
+
+(defn dict-words []
+  (clojure.string/split (slurp "/usr/share/dict/words") #"\n"))
+
+(deftest test-read-dict
+  (let [w (dict-words)]
+    (is (= 235886 (count (dict-words))))
+    (is (= "A" (first (dict-words))))
+    (is (= "Zyzzogeton" (last (dict-words))))))
+
+(deftest test-big-test
+  (testing "reads lots of words"
+    (let [t (insert-words (dict-words))]
+      (is (= 235886 (count (words t)))))))
